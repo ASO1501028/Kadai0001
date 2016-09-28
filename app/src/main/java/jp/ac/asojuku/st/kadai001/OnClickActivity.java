1ba7a0cb01c25fdb54c51a8999d0d31d3abe631f
masatokg <masatokg@users.noreply.github.com>
2016-09-28 13:40:39 +0900

    課題提出用

--- app/src/main/java/jp/ac/asojuku/st/kadai001/OnClickActivity.java
+++ app/src/main/java/jp/ac/asojuku/st/kadai001/OnClickActivity.java
@@ -19,32 +19,34 @@ implements View.OnClickListener{
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
 
         // 前の画面からのインテントを取得
         Intent preIntent = getIntent();
+        preIntent.getExtras();
+
         // 選択肢を表すキーワードを文字列定義リソースから取得
         String keyword = getResources().getString(R.string.usersOption);
         // 選択された文字列をキーワードを元に取得
-        String usersOption = (String)preIntent.getExtras().get(keyword);
+        String answer = (String) preIntent.getStringExtra(keyword);
 
         // 正解を文字列定義リソースから取得
-        String correctWord = getResources().getString(R.string.usagi);
+        String correctWord = (String) getResources().getString(R.string.usagi);
 
-//        String answer =
+
         // テキストビューの表示はデフォルトは不正解にしてあるので、正解時に表示を変更
-        if( usersOption.equals(correctWord) ){
-            TextView tv = (TextView)this.findViewById(R.id.tvAnsMsg);
-            tv.setText(R.string.seikaiMsg);
+        if (answer.equals(correctWord)){
+        TextView text = (TextView) this.findViewById(R.id.tvAnsMsg);
+        text.setText(getResources().getString(R.string.seikaiMsg));
+
         }
 
     }
 
-
+    // 戻るボタンの動作
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(OnClickActivity.this, MainActivity.class);
-        startActivity(intent);
+        finish();
     }
 }
