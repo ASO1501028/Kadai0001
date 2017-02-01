4db3d960fb2e127f6da0ad1c60993c0aaab640ec
AHKNT <nf700v1010@gmail.com>
2017-02-01 16:49:58 +0900

    再提出

--- app/src/main/java/jp/ac/asojuku/st/kadai001/OnLongClickActivity.java
+++ app/src/main/java/jp/ac/asojuku/st/kadai001/OnLongClickActivity.java
@@ -1,19 +1,24 @@
 package jp.ac.asojuku.st.kadai001;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
-public class OnLongClickActivity extends AppCompatActivity {
+public class OnLongClickActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_on_long_click);
         Button btnBack = (Button) this.findViewById(R.id.btnBack02);
+        btnBack.setOnClickListener(this);
 
 
     }
 
-    // 戻るボタンの動作
+    @Override
+    public void onClick(View v) {
+        finish();
+    }
 }
